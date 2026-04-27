581bb55511396ff1e9daed7ce3b5d76eed75d3ec
truthless-dev <truthless.deviant248@simplelogin.fr>
2026-04-27 15:58:42 -0700

    chore: initial commit

---
+++ Package.swift
@@ -0,0 +1,33 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+  name: "Define",
+  platforms: [
+    .macOS(.v12)
+  ],
+  products: [
+    .executable(
+      name: "define",
+      targets: [
+        "Cli"
+      ]
+    )
+  ],
+  dependencies: [
+    .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0"),
+    .package(url: "https://github.com/swiftlang/swift-docc-plugin", from: "1.1.0"),
+  ],
+  targets: [
+    // Targets are the basic building blocks of a package, defining a module or a test suite.
+    // Targets can depend on other targets in this package and products from dependencies.
+    .executableTarget(
+      name: "Cli",
+      dependencies: [
+        .product(name: "ArgumentParser", package: "swift-argument-parser")
+      ]
+    )
+  ]
+)
---
+++ README.md
@@ -0,0 +1,11 @@
+# Define
+
+Get definitions of English words from the command line
+
+## Dictionary Source
+
+This program utilizes the [Free Dictionary API](free-dictionary-api) to obtain all of its definitions, phonetics, synonyms, and antonyms. All credit and thanks should be directed toward the owner of that generous project.
+
+Users should please endeavor not to overwhelm that server with requests. If you need to check a dictionary so often, maybe you should buy a physical one to keep at your desk!
+
+[free-dictionary-api]: https://dictionaryapi.dev/
